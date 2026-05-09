e2bba2fdb0f8cc99cd0dd772ee46d1a38fe8c68e
leeminkyu-kr96 <leewkdrns2@gmail.com>
2026-05-09 10:54:58 +0900

    프론트 호출 테테스트

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ origins = [
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # 허용할 도메인 리스트
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],  # GET, POST 등 모든 메소드 허용
     allow_headers=["*"],  # 모든 헤더 허용
 )
